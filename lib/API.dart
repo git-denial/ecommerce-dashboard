3ea7dd862a80f961d4b9e720fb3e49d241c3d80c
diundang <danielundang@gmail.com>
2021-12-15 08:45:23 +0700

    Fix header

--- lib/API.dart
+++ lib/API.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'package:admin/models/Authentication.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -10,15 +11,15 @@ final String baseUrl = 'http://localhost:10017';
 Function apiRequest = (route, method, {body}) async {
   print(body);
   final Map<String, String> header = {
-  'Content-Type': 'application/json; charset=UTF-8',
-  'Authorization': 'Bearer ${Authentication.token}',
+  HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+  HttpHeaders.authorizationHeader: 'Bearer ${Authentication.token}',
 };
   try {
     var response;
 
     switch (method) {
       case "GET":
-        response = await http.get(Uri.parse('${baseUrl}/${route}'));
+        response = await http.get(Uri.parse('${baseUrl}/${route}'),headers: header);
         break;
       case "POST":
         response = await http.post(
